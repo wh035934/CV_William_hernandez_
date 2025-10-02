a25db7953d9b276b407a9f275277a6764e4e99c4
William Isaac <wh035934@gmail.com>
2025-10-02 15:09:12 -0600

    Subo CV actualizado y mejoras al portafolio

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ toggleDarkModeBtn.addEventListener('click', () => {
 if (localStorage.getItem('darkMode') === 'true') {
     document.body.classList.add('dark-mode');
 }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btnArriba");
+    const seccion = document.getElementById("proyectos"); // Asegúrate que esta sección exista
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > seccion.offsetTop) {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+    });
+
+    btn.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  });
